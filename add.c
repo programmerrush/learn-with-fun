e0649d4ba9ad41b3e7039531b298e2cde484f41d
Rushikesh <programmerrush@gmail.com>
2022-03-27 22:47:02 +0530

    Optimized addition program

--- add.c
+++ add.c
@@ -1,11 +1,8 @@
-/* Version - 1 */
+/* Version - 1.1 */
 
 #include <stdio.h>
-
 int main(){
     int a=3, b=5;
-
-    printf("A + B = %d", a+b);
-
+    printf("%d + %d = %d", a, b, a+b);
     return 0;
 }
